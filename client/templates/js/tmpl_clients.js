79ed8ea1555e20d38aeef2f8c93ce4004db24198
Yury <zentelechia@gmail.com>
2014-04-28 15:22:25 +0400

    ctrl+click - hide client

--- client/templates/js/tmpl_clients.js
+++ client/templates/js/tmpl_clients.js
@@ -7,12 +7,18 @@ Template.clients.client=function(){
 		return  getClients(Session.get("query"));
 };
 Template.clients.events({
-		'click .clients div' : function() {
-			cid=$(event.currentTarget)[0].id;
-			path=Router.path("client",{id: cid});
-			link=Clients.findOne({_id: cid}).workName;
-			pushHistory(path,link);
-			Router.go('client', {id : cid});
+		'click .clients div' : function(event) {
+				cid=$(event.currentTarget)[0].id;
+			if (event.ctrlKey){
+				Clients.update(cid, {$set : {hidden : true}});
+			}
+			else {
+				
+				path=Router.path("client",{id: cid});
+				link=Clients.findOne({_id: cid}).workName;
+				pushHistory(path,link);
+				Router.go('client', {id : cid});
+			}
 
 		}
 	});
